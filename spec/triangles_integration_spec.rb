93e2eab7b04730e43fbc3512b5c60fea95367c65
Jonathan Thom and Tracie Weitzman <jonathan@>
2016-11-28 09:06:06 -0800

    passes second integration spec, testing for false triangle sides

--- spec/triangles_integration_spec.rb
+++ spec/triangles_integration_spec.rb
@@ -12,4 +12,12 @@ describe('The triangles path', {:type => :feature}) do
     click_button('Trianglize')
     expect(page).to have_content("scalene")
   end
+  it ("takes inputs for three sides to determine type of triangle") do
+    visit('/')
+    fill_in('side1', :with => '3')
+    fill_in('side2', :with => '3')
+    fill_in('side3', :with => '33')
+    click_button('Trianglize')
+    expect(page).to have_content("Those side lengths don't make a triangle")
+  end
 end
